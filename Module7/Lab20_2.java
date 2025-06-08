b7853de422976267a14164910d4444c41abbc653
V Smol <96552183+InjuSmol@users.noreply.github.com>
2025-06-08 16:52:51 -0400

    Update Lab20_2.java

--- Module7/Lab20_2.java
+++ Module7/Lab20_2.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-// Smolianova Varvara 115284311
+
 public class Lab20_2 {
     public static void main(String[] args) {
         Course course1 = new Course("Data Structures");
